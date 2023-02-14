0fc18455270494323d8eebe6c0c46164054e730c
chrispoturalski <chrispotural@gmail.com>
2023-02-14 09:24:50 -0800

    added routes

--- routes/api/thought-routes.js
+++ routes/api/thought-routes.js
@@ -4,37 +4,114 @@ const { Thought, Reaction} = require('../../models')
 
 //TODO: ROUTE TO GET ALL THOUGHTS
 router.get('/', (req,res)=> {
-
+    Thought.find({}, (err, thoughts) => {
+        res.status(200).json(thoughts)
+    });
 })
 
 //TODO: ROUTE TO CREATE A NEW THOUGHT
 router.post('/', (req,res)=> {
-
+    Thought.create(
+        {
+            thoughtText: req.body.thoughtText,
+            username: req.body.username
+        },
+        (err, thought) => {
+            if (err) {
+                res.status(500).json(err);
+            } else {
+                res.status(200).json(thought);
+            }
+        }   
+    )
 });
 
 //TODO: ROUTE TO GET SINGLE THOUGHT BASED ON THOUGHT ID
 router.get('/:thoughtId', (req,res)=> {
-
+    Thought.findById(req.params.thoughtId, (err, thought) => {
+        if (err) {
+            res.status(500).json("No thought found");
+        } else {
+            res.status(200).json(thought);
+        }
+    }
+    )
 })
 
 //TODO: ROUTE TO UPDATE A THOUGHT
 router.put('/', (req,res)=> {
-
+    Thought.findByIdAndUpdate(req.params.thoughtId, 
+        {
+            thoughtText: req.body.thoughtText,
+            username: req.body.username
+        },
+        (err, thought) => {
+            if (err) {
+                res.status(500).json("No thought found");
+            } else {
+                res.status(200).json(thought);
+            }
+        }
+    )
 })
 
 //TODO: ROUTE TO DELETE A THOUGHT BASED ON THOUGHT ID
 router.delete('/:thoughtId', (req,res)=> {
-
+    Thought.findByIdAndDelete(req.params.thoughtId, (err, thought) => {
+        if (err) {
+            res.status(500).json("No thought found");
+        } else {
+            res.status(200).json(thought);
+        }
+    })
 });
 
 //TODO: ROUTE TO ADD REACTION TO A THOUGHT
 router.post('/:thoughtId/reactions', (req,res)=> {
-
+    Thought.findByIdAndUpdate(req.params.thoughtId, 
+        {
+            $push: {
+                reactions: {
+                    reactionId: req.body.reactionId,
+                    reactionBody: req.body.reactionBody,
+                    username: req.body.username
+                }
+            }
+        },
+        {
+            new: true
+        },
+        (err, thought) => {
+            if (err) {
+                res.status(500).json("No thought found");
+            } else {
+                res.status(200).json(thought);
+            }
+        }
+    )
 });
 
 //TODO: ROUTE TO DELETE A REACTION ON A THOUGHT
 router.delete('/:thoughtId/reactions/:reactionId', (req,res)=> {
-
+    Thought.findByIdAndUpdate(req.params.thoughtId, 
+        {
+            $pull: {
+                reactions: {
+                    reactionId: req.params.reactionId
+                }
+            }
+        },
+        {
+            new: true
+        },
+        (err, thought) => {
+            if (err) {
+                res.status(500).json("No thought found");
+            } else {
+                res.status(200).json(thought);
+            }
+        }
+    )
 })
 
 module.exports = router;
--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -4,27 +4,67 @@ const {User} = require("../../models")
 
 //TODO - ROUTE THAT GETS ALL THE USERS, include friends?
 router.get('/', (req,res)=> {
-
-})
+    User.find({}, (err, users) => {
+        res.status(200).json(users)
+    });
+});
 
 //TODO - ROUTE THAT CREATES A NEW USER
 router.post('/', (req,res)=> {
-
+    User.create(
+        {
+            username: req.body.username,
+            email: req.body.email
+        },
+        (err, user) => {
+            if (err) {
+                res.status(500).json(err);
+            } else {
+                res.status(200).json(user);
+            }
+        }   
+    )
 });
 
 //TODO - ROUTE THAT GETS A SINGLE USER BASED ON USER ID
 router.get('/:userId', (req,res) => {
-
+    User.findById(req.params.userId, (err, user) => {
+        if (err) {
+            res.status(500).json("No user found");
+        } else {
+            res.status(200).json(user);
+        }
+    }
+    )
 })
 
 //TODO - ROUTE THAT UPDATES A SINGLE USER
 router.put('/:userId', (req,res)=> {
-
+    User.findByIdAndUpdate(req.params.userId, 
+        {
+            username: req.body.username,
+            email: req.body.email
+        },
+        (err, user) => {
+            if (err) {
+                res.status(500).json("No user found");
+            } else {
+                res.status(200).json(user);
+            }
+        }
+    )
 })
 
 //TODO - ROUTE THAT DELETES A SINGLE USER BASED ON USER ID
 router.delete('/:userId', (req,res)=> {
-
+    User.findByIdAndDelete(req.params.userId, (err, user) => {
+        if (err) {
+            res.status(500).json("No user found");
+        } else {
+            res.status(200).json(user);
+        }
+    }
+    )
 });
 
 //TODO - ROUTE THAT ADDS A FRIEND TO A USER

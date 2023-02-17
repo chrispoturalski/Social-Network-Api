53b4859a99ca3b9ccbaa8925166fc761864e0c8a
chrispoturalski <chrispotural@gmail.com>
2023-02-16 22:51:12 -0800

    added dotenv

--- server.js
+++ server.js
@@ -1,10 +1,14 @@
-
+const dotenv = require('dotenv');
+dotenv.config();
 const express = require('express');
 const db = require('./config/connection');
 const routes = require('./routes');
+// const mongodb = require('mongodb').MongoClient;
 
 const PORT = process.env.PORT || 3001;
 const app = express();
+
+// const connectionStringURI = process.env.MONGODB_URI || 'mongodb://localhost/social-network-api';
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
